bb68c0b449a8ee867da570b1cef872d96c9ac296
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2015-02-11 16:31:24 -0500

    Fixed example.

--- Example.hs
+++ Example.hs
@@ -1,8 +1,8 @@
-module SmtLib.Example where
+module Example where
 
-import SmtLib.Parsers.CommandsParsers
+import Smtlib.Parsers.CommandsParsers
 import           Text.ParserCombinators.Parsec as Pc
-import SmtLib.Syntax.Syntax
+import Smtlib.Syntax.Syntax
 
 parseSmtLibFile :: IO ()
 parseSmtLibFile =  getLine >>= readFile >>= pt.parse parseSource ""
