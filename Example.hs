09e91542a0caef8d22ada3c054cd45f74cbd454a
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2015-02-11 16:32:27 -0500

    Cosmetic changes.

--- Example.hs
+++ Example.hs
@@ -1,16 +1,15 @@
 module Example where
 
 import Smtlib.Parsers.CommandsParsers
-import           Text.ParserCombinators.Parsec as Pc
 import Smtlib.Syntax.Syntax
+import Text.ParserCombinators.Parsec
 
 parseSmtLibFile :: IO ()
-parseSmtLibFile =  getLine >>= readFile >>= pt.parse parseSource ""
-
+parseSmtLibFile = getLine >>= readFile >>= pt.parse parseSource ""
 
 pt :: Either ParseError Source -> IO ()
 pt (Left err) = print err
-pt (Right x) = pt' x
+pt (Right x)  = pt' x
 
-pt' :: Source -> IO()
+pt' :: Source -> IO ()
 pt' = foldr (\ x -> (>>) (print x >> putStr "\n")) (return ())
