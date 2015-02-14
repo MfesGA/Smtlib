11562bcded85f0fcd344266ce55ed281d6b3f3c3
Rogério Pontes <rogerp62@outlook.com>
2015-02-14 15:41:29 +0000

    update ShowSL

--- Smtlib/Syntax/ShowSL.hs
+++ Smtlib/Syntax/ShowSL.hs
@@ -3,6 +3,10 @@ module Smtlib.Syntax.ShowSL where
 import Data.List 
 import Smtlib.Syntax.Syntax
 
+
+joinA ::(ShowSL a) => [a] -> String 
+joinA = (intercalate " ").(fmap showSL)
+
 class ShowSL a where
   showSL :: a -> String
 
@@ -10,38 +14,27 @@ instance ShowSL Command where
   showSL (SetLogic s) = "(set-logic " ++ s ++ ")"
   showSL (SetOption opt) = "(set-option " ++ showSL opt ++ ")"
   showSL (SetInfo info) = "(set-info " ++ showSL info ++ ")"
-  showSL (DeclareSort str val) = 
-    "(declare-sort " ++ str ++ " " ++ show val ++ ")"
-  showSL (DefineSort str strs sort) = "(define-sort " ++ str ++ " ( " ++
-      joinA sort ++ " ) " ++ showSL sort ++ ") "
-    where joinA x = (intercalate " ").fmap showSL x
+  showSL (DeclareSort str val) =  "(declare-sort " ++ str ++ 
+    " " ++ show val ++ ")"
+  showSL (DefineSort str strs sort) = "(define-sort " ++ str ++ 
+    " ( " ++ (joinA strs) ++ " ) " ++ showSL sort ++ ") "
+  showSL (DeclareFun  str sorts sort) = "(declare-sort  " ++ str ++ 
+    " ( "  ++ joinA sorts ++ " )  " ++ showSL sort ++ ") "
+  showSL (DefineFun str srvs sort term) = "( define-sort"   ++ str ++ 
+    " (  " ++ joinA srvs ++ " ) " ++ showSL sort ++ " " ++ showSL term ++ " )"
+  showSL (Push n) = "(push " ++ show n ++ " )"
+  showSL (Pop n) = "(pop " ++show n ++ " )"
+  showSL (Assert term) = "(asert " ++ showSL term ++ ") "
+  showSL CheckSat = "(check-sat)" 
+  showSL GetAssertions = "(get-assertions)"
+  showSL GetProof = "(get-proof)"
+  showSL GetUnsatCore = "(get-unsat-core)"
+  showSL (GetValue terms) = "( (" ++ joinA terms ++ ") )"
+  showSL GetAssignment =  "(get-assignment)"
+  showSL (GetOption opt) = "(get-option " ++ opt ++ ")"
+  showSL (GetInfo info) = "(get-info " ++ showSL info ++ " )"
+  showSL Exit = "(exit)"
 
-{-
-  SetLogic s = "(set-logic " ++ showSL s ++ ")"
-  SetOption opt =  "(set-option " ++ showSL opt ++ ")"
-  SetInfo info = "(set-info " ++ showSL info ++ ")"
-  DeclareSort str val = 
-    "(declare-sort " ++ showSL str ++ " " ++ showSL val ++")"
-  DefineSort str vals sort = 
-    "(define-sort " ++  str ++ " ( " ++ joinA vals ++ " )" ++ 
-      showSL sort ++ ")"
-  DeclareFun str sorts sort = 
-    "(declare-fun " ++ str ++ " ( " ++ joinA sorts ++ " )" ++
-      showSl sort ++ ")" 
-  DefineFun str srvs sort term = 
-    "(define-fun " ++ str ++ " ( " ++ joinA srvs ++ " ) " ++ 
-      showSL sort ++ " " ++  showSL term ++ ")"
-  Push n = "(push " ++ show n ++ ")"
-  Pop n = "(pop " ++ show n ++ ")"
-  Assert term = "(assert " ++ showSL term ++ ")"
-  CheckSat = "(check-sat)"
-  GetAssertions = "(get-assertions)"
-  GetProof = "(get-proof)"
-  GetUnsatCore = "(get-unsat-core)"
-  GetValue terms = "( (" ++ joinA terms ++ ") )"
-  GetAssignment =  "(get-assignment)"
-  GetOption opt = "(get-option " ++ opt ++ ")"
-  GetInfo info = "(get-info " ++ showSL info ++ " )"
-  Exit = "(exit)"
-  where joinA x = (intercalate " ").fmap showSL x
--}
+
+instance ShowSL Option where
+  showSL (PrintSucess b) = "(print-sucess " ++ show b ++ " )"
